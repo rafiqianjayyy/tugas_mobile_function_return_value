92cdb8eed231de2ece5353827790332ac44607f3
rafiqi <muhammadrafiqi171@gmail.com>
2024-10-12 22:34:58 +0700

    Create function return value.dart

---
+++ function return value.dart
@@ -0,0 +1,13 @@
+//INI ADALAH CONTOH FUNCTION RETURN VALUE
+// int Tambah(List<int> bilangan){
+//   var hasil = 0;
+//   for(var value in bilangan){
+//     hasil += value;
+//   }
+//   return hasil;
+// }
+
+// void main(){
+//   print(Tambah([10,10,10,10]));
+//   print(Tambah([1,1,1,1,1]));
+// }
